85ab9cf46dacfa81567785f2e81faa5daa046528
Misha Khapalnov <michkahap@gmail.com>
2023-07-10 15:08:40 +0300

    Create User package

--- Server/src/main/java/com/rocket/Server/ServerApplication.java
+++ Server/src/main/java/com/rocket/Server/ServerApplication.java
@@ -1,4 +1,4 @@
-package com.rocket.Server;
+package com.rocket.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ Server/src/main/java/com/rocket/Server/user/Role.java
@@ -0,0 +1,8 @@
+package com.rocket.server.user;
+
+public enum Role {
+    ADMIN,
+    TEACHER,
+    STUDENT,
+    USER
+}
