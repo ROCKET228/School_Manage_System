3ce6df4609f645097be16b33634f2ffaeec68695
Misha Khapalnov <michkahap@gmail.com>
2023-11-09 17:01:20 +0200

    Create test folder for admin

---
+++ src/test/java/com/rocket/admin/AdminControllerTest.java
@@ -0,0 +1,88 @@
+package com.rocket.admin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminControllerTest {
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void getAllClass() {
+    }
+
+    @Test
+    void getAllSubject() {
+    }
+
+    @Test
+    void getAllMarks() {
+    }
+
+    @Test
+    void setTeacherRole() {
+    }
+
+    @Test
+    void setStudentRole() {
+    }
+
+    @Test
+    void setStudentToClass() {
+    }
+
+    @Test
+    void setTeacherToSubject() {
+    }
+
+    @Test
+    void unsetStudentFromClass() {
+    }
+
+    @Test
+    void unsetTeacherFromSubject() {
+    }
+
+    @Test
+    void changeTeacherInMarksTable() {
+    }
+
+    @Test
+    void createStudent() {
+    }
+
+    @Test
+    void createTeacher() {
+    }
+
+    @Test
+    void createClasses() {
+    }
+
+    @Test
+    void createSubject() {
+    }
+
+    @Test
+    void createMarks() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void deleteClasses() {
+    }
+
+    @Test
+    void deleteSubject() {
+    }
+
+    @Test
+    void deleteMarks() {
+    }
+}
---
+++ src/test/java/com/rocket/admin/AdminServiceTest.java
@@ -0,0 +1,88 @@
+package com.rocket.admin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminServiceTest {
+
+    @Test
+    void setTeacherRole() {
+    }
+
+    @Test
+    void setStudentRole() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void createStudent() {
+    }
+
+    @Test
+    void createTeacher() {
+    }
+
+    @Test
+    void createClasses() {
+    }
+
+    @Test
+    void setStudentToClass() {
+    }
+
+    @Test
+    void createSubject() {
+    }
+
+    @Test
+    void setTeacherToSubject() {
+    }
+
+    @Test
+    void createMarks() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void deleteClasses() {
+    }
+
+    @Test
+    void deleteSubject() {
+    }
+
+    @Test
+    void deleteMarks() {
+    }
+
+    @Test
+    void unsetTeacherFromSubject() {
+    }
+
+    @Test
+    void unsetStudentFromClass() {
+    }
+
+    @Test
+    void changeTeacherInMarksTable() {
+    }
+
+    @Test
+    void getAllClass() {
+    }
+
+    @Test
+    void getAllSubject() {
+    }
+
+    @Test
+    void getAllMarks() {
+    }
+}
