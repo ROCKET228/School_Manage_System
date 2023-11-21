df8268a656708778e41a4fa17d1dbebcc862238a
Misha Khapalnov <michkahap@gmail.com>
2023-11-21 16:49:02 +0200

    Make class repository test

--- src/test/java/com/rocket/classes/ClassRepositoryTest.java
+++ src/test/java/com/rocket/classes/ClassRepositoryTest.java
@@ -1,16 +1,63 @@
 package com.rocket.classes;
 
+import com.rocket.auth.RegisterRequest;
+import com.rocket.user.User;
+import com.rocket.user.UserRepository;
+import com.rocket.user.UserRole;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class ClassRepositoryTest {
+
+    @Autowired
+    private ClassRepository classRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private Class classes;
+
+    private User student;
+
+    private String className = "11-B";
+
+    @BeforeEach
+    public void init(){
+        classes = Class.builder().name(className).build();
+        student = new User(1, "Name", "lastName", "user@mail.com", "12345", UserRole.STUDENT);
+    }
 
     @Test
     void findByName() {
+        classRepository.save(classes);
+        Class savedClass = classRepository.findByName(className).orElseThrow();
+        assertEquals(savedClass, classes);
     }
 
     @Test
     void findByEnrolledStudents() {
+        userRepository.save(student);
+        Set<User> enrolledStudents = new HashSet<>();
+        enrolledStudents.add(student);
+        classes.setEnrolledStudents(enrolledStudents);
+        classRepository.save(classes);
+        Class savedClass = classRepository.findByEnrolledStudents(student).orElseThrow();
+        assertEquals(savedClass, classes);
     }
 }
