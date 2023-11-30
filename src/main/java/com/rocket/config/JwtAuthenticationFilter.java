132ca2aac22f958d0aabe0124852f75dee595da7
Misha Khapalnov <michkahap@gmail.com>
2023-11-30 18:59:34 +0200

    Make some updates

--- src/main/java/com/rocket/config/JwtAuthenticationFilter.java
+++ src/main/java/com/rocket/config/JwtAuthenticationFilter.java
@@ -7,6 +7,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
