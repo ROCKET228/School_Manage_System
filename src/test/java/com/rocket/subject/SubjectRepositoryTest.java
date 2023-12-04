a6eb86437f6de7ba2906d5b5802d10a53e048501
Misha Khapalnov <michkahap@gmail.com>
2023-12-04 18:27:27 +0200

    Make docker compose file for server

--- src/test/java/com/rocket/subject/SubjectRepositoryTest.java
+++ src/test/java/com/rocket/subject/SubjectRepositoryTest.java
@@ -1,9 +1,5 @@
 package com.rocket.subject;
 
-
-import com.rocket.user.User;
-import com.rocket.user.UserRepository;
-import com.rocket.user.UserRole;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import java.util.HashSet;
 import java.util.Optional;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,19 +17,14 @@ class SubjectRepositoryTest {
     @Autowired
     private SubjectRepository subjectRepository;
 
-    @Autowired
-    private UserRepository userRepository;
-
     private Subject subject;
 
-    private User teacher;
 
     private final String subjectName = "Math";
 
     @BeforeEach
     public void init(){
         subject = Subject.builder().name(subjectName).build();
-        teacher = new User(1, "Name", "lastName", "user@mail.com", "12345", UserRole.TEACHER);
     }
 
     @Test
@@ -55,14 +43,4 @@ class SubjectRepositoryTest {
         Assertions.assertThat(returnSubject).isEmpty();
     }
 
-    @Test
-    void findAllByEnrolledTeachers() {
-        userRepository.save(teacher);
-        Set<User> enrolledTeacher = new HashSet<>();
-        enrolledTeacher.add(teacher);
-        subject.setEnrolledTeachers(enrolledTeacher);
-        subjectRepository.save(subject);
-        Subject savedSubject = subjectRepository.findByName(subjectName).orElseThrow();
-        assertEquals(savedSubject, subject);
-    }
 }
